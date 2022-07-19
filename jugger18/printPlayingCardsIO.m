de421bfe3d320ae269655aebfffa0c2010385dbf
itsanibondip <paolo.sanibondi@tetrapak.com>
2022-07-19 12:22:28 +0200

    cardorizzv1

--- jugger18/printPlayingCardsIO.m
+++ jugger18/printPlayingCardsIO.m
@@ -2,15 +2,16 @@ clc
 close all
 clear
 
-fileID = fopen("J18_CSVfile.csv",'w');
+fileID = fopen("JuggerCardOrizzv1_CSVfile.csv",'w');
 
-N = 18;
-title = "J18";
+N = 20;
+title = "Jugger";
 fprintf(fileID,"master\n");
 
 for i = 1:N
-    tmp = sprintf("card%03d.png\n",i)
-    text = strcat("https://psanibondi.github.io/jugger18/J18v1/sets/J18v1/",tmp);
+    %tmp = sprintf("card%03d.png\n",i)
+    tmp = sprintf("CardOrizzv1_Page_%02d.png\n",i)
+    text = strcat("https://psanibondi.github.io/jugger18/JuggerCardOrizzv1/",tmp);
     fprintf(fileID,text);
 end
 fclose(fileID);
